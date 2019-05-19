9ff680d222a625139e4bf067b74c130e08089104
ariefhirmanto <ariefhirmanto7@gmail.com>
2019-05-19 15:34:26 +0700

    finished project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 import firebase from 'firebase';
 import { View } from 'react-native';
-import { Header } from './components/common';
+import { Header, Button, Spinner, Card, CardSection } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			loggedIn: null,
+		};
+	}
+
 	componentWillMount() {
 		firebase.initializeApp({
 			apiKey: 'AIzaSyD8IeTf7Kk3Me7jfMQkwcjtbw1RkeomhoU',
@@ -15,16 +23,54 @@ class App extends Component {
 			messagingSenderId: '178579573995',
 			appId: '1:178579573995:web:2ba365895d0ab93b'
 		});
+		firebase.auth().onAuthStateChanged((user) => {
+			if (user) {
+				this.setState({ loggedIn: true });
+			} else {
+				this.setState({ loggedIn: false });
+			}
+		});
+	}
+
+	renderContent() {
+		switch (this.state.loggedIn) {
+			case true:
+				return (
+					<Card >
+						<CardSection>
+							<Button onPress={() => firebase.auth().signOut()} >
+							Log Out
+							</Button>
+						</CardSection>
+					</Card>
+				);
+			case false:	
+				return (
+					<LoginForm />
+				);
+			default:
+				return (
+				<View style={styles.dropdownContent}>
+					<Spinner size="large" style={styles.dropdownContent} />
+				</View>
+				);
+		}
 	}
 
 	render() {
 		return (
 			<View>
 				<Header headerText="Authentication" />
-				<LoginForm />
+				{this.renderContent()}
 			</View>
 		);
 	}
 }
+
+const styles = {
+	dropdownContent: {
+		marginTop: 20
+	}
+};
 
 export default App;
